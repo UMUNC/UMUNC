4c55086645fae2acf8e1f4018fcdee117f37e4de
eastpiger <lvwt@shanghaitech.edu.cn>
2015-04-20 02:11:01 +0800

    import part_upload

--- umunc_iris/view.py
+++ umunc_iris/view.py
@@ -225,7 +225,7 @@ def step3(request):
 	Rmsg=''
 	if request.POST.has_key('submit'):
 		file_path='%s_%s' %(time.time(),string.join(random.sample(['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a','1','2','3','4','5','6','7','8','9','0'], 6)).replace(' ',''))
-		result=upload('/www/upload/review/'+file_path,request.FILES['upload_file'].name,'upload_file')
+		result=part_upload.upload('/www/upload/review/'+file_path,request.FILES['upload_file'].name,'upload_file')
 		if result:
 			request.user.profile.Review=file_path+'/'+request.FILES['upload_file'].name
 			request.user.profile.save()
