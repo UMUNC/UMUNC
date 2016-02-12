b8fd288a4c343174ea6cf6facd141d73cea75c01
eastpiger <lvwt@shanghaitech.edu.cn>
2016-02-13 00:45:56 +0800

    fix

--- loader.sh
+++ loader.sh
@@ -1,5 +1,5 @@
 python2 makeconf.py > umunc/CONFIG.py
 python2 manage.py migrate
 
-nohup gunicorn umunc.wsgi:application -b 127.0.0.1:8080&
 service nginx start
+nohup gunicorn umunc.wsgi:application -b 127.0.0.1:8080
