e0c6078e8ba415276213c44ea15b5ca45b8352f0
eastpiger <lvwt@shanghaitech.edu.cn>
2016-02-15 09:45:04 +0800

    add workers

--- loader.sh
+++ loader.sh
@@ -2,7 +2,7 @@ python2 makeconf.py > umunc/CONFIG.py
 python2 manage.py makemigrations
 python2 manage.py migrate
 
-nohup gunicorn umunc.wsgi:application -b 127.0.0.1:8080&
+nohup gunicorn umunc.wsgi:application -b 127.0.0.1:8080 --workers 4 --worker-connections 65535&
 service nginx start
 
 while true
