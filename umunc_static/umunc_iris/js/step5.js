e2543ef4f037021e1030e8b07ec8e888ed637dc1
eastpiger <lvwt@shanghaitech.edu.cn>
2016-03-15 01:05:32 +0800

    update

---
+++ umunc_static/umunc_iris/js/step5.js
@@ -0,0 +1,5 @@
+$(function() {
+	$(".process_panel_item:nth-child(6)").removeClass("success");
+	$(".process_panel_item:nth-child(6)").removeClass("next");
+	$(".process_panel_item:nth-child(6)").addClass("active");
+});
