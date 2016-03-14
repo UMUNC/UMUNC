384e142d13cd736817d517e336d8488207f7def8
eastpiger <lvwt@shanghaitech.edu.cn>
2016-03-15 03:02:38 +0800

    update

--- umunc_iris/admin.py
+++ umunc_iris/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
 from umunc_iris.models import *
 
+class GroupAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Basic', {
+            'fields': ('Name', 'School', 'Password')
+        }),
+        ('Status', {
+            'fields': ('Paycode', 'Payment', 'Group')
+        }),
+        ('Interviewer', {
+            'classes': ('collapse'),
+            'fields': ('sendmail',)
+        }),
+    )
+    list_display = ('User', 'Name','Status')
+
+    readonly_fields = ('TimeStamp', 'LastMotified', 'sendmail')
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.has_perm('profile.control_all'):
+            return ('TimeStamp', 'LastMotified')
+        else:
+            return ('TimeStamp', 'LastMotified')
+    def sendmail(self, obj):
+        return django.utils.html.escape(u'''
+            <a href=\"/iris/admin/sendmail/?command=sendmail_payment&id='''+obj.id+u'''\">发送缴费确认邮件</a>
+            ''')
+
 class ProfileAdmin(admin.ModelAdmin):
     fieldsets = (
         ('Status', {
@@ -36,13 +62,13 @@ class ProfileAdmin(admin.ModelAdmin):
             return ('TimeStamp', 'LastMotified')
     def sendmail(self, obj):
         return django.utils.html.escape(u'''
-            <a href="/iris/admin/sendmail/?command=sendmail_emailcheck&id='''+obj.User.id+u'''">发送注册邮件</a><br/>
-            <a href="/iris/admin/sendmail/?command=sendmail_interview&id='''+obj.User.id+u'''">发送面试通知邮件</a><br/>
-            <a href="/iris/admin/sendmail/?command=sendmail_identify&id='''+obj.User.id+u'''">发送席位通知邮件</a><br/>
-            <a href="/iris/admin/sendmail/?command=sendmail_payment_user&id='''+obj.User.id+u'''">发送缴费确认邮件</a>
+            <a href=\"/iris/admin/sendmail/?command=sendmail_emailcheck&id='''+obj.User.id+u'''\">发送注册邮件</a><br/>
+            <a href=\"/iris/admin/sendmail/?command=sendmail_interview&id='''+obj.User.id+u'''\">发送面试通知邮件</a><br/>
+            <a href=\"/iris/admin/sendmail/?command=sendmail_identify&id='''+obj.User.id+u'''\">发送席位通知邮件</a><br/>
+            <a href=\"/iris/admin/sendmail/?command=sendmail_payment_user&id='''+obj.User.id+u'''\">发送缴费确认邮件</a>
             ''')
 
-admin.site.register(group)
+admin.site.register(group, GroupAdmin)
 admin.site.register(profile, ProfileAdmin)
 admin.site.register(checkcode)
 admin.site.register(country)
