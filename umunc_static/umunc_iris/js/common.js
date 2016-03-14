b85d619a39a831ff7657dd3bb457069b974c3bf6
eastpiger <lvwt@shanghaitech.edu.cn>
2016-03-15 00:59:26 +0800

    update

--- umunc_static/umunc_iris/js/common.js
+++ umunc_static/umunc_iris/js/common.js
@@ -2,10 +2,12 @@
 * @Author: eastpiger
 * @Date:   2016-03-14 01:14:08
 * @Last Modified by:   eastpiger
-* @Last Modified time: 2016-03-14 22:44:43
+* @Last Modified time: 2016-03-15 00:56:13
 */
-$(".process_panel_item.success .process_panel_icon").html("<span class=\"fui-check\"></span>");
-$(".process_panel_item.next .process_panel_icon").html("<span class=\"fui-arrow-right\"></span>");
-$(".process_panel_item.success,.process_panel_item.next,.process_panel_item.active").click(function(event) {
-		window.location.href=$(this).attr("href");
-	});
+$(function() {
+	$(".process_panel_item.success .process_panel_icon").html("<span class=\"fui-check\"></span>");
+	$(".process_panel_item.next .process_panel_icon").html("<span class=\"fui-arrow-right\"></span>");
+	$(".process_panel_item.success,.process_panel_item.next,.process_panel_item.active").click(function(event) {
+			window.location.href=$(this).attr("href");
+		});
+})
