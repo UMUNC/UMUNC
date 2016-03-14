0d72ecef4329193c980c04e3dcff8276355f0506
eastpiger <lvwt@shanghaitech.edu.cn>
2016-03-15 01:41:05 +0800

    update

--- umunc/part_mail.py
+++ umunc/part_mail.py
@@ -4,10 +4,39 @@ from django.core.mail import EmailMessage
 def sendmail(to,subject,html):
 	email = EmailMessage(
 		subject= subject,
-		html = html,
+		body = html,
 		to = [to])
 	email.content_subtype = "html"
 	email.send()
 
 def sendmail_emailcheck(to,username,code):
-	sendmail(to,username+u"，快来验证您的邮箱吧！",u"<h3>尊敬的用户 "+username+u" 您好：</h3><p>您收到此邮件是因为此邮箱（ "+to+u" ）正在注册“UMUNC IRIS”的账号。</p><p>如果这不是您的操作，请直接忽视本邮件；如果您确认这是您的操作，请点击下面的地址或将其复制到浏览器地址栏并进入，即可验证您的邮箱，激活您的账号：</p><h4><a href=\"http://121.41.24.81/iris/accounts/check?checkcode="+code+u"\">http://121.41.24.81/iris/accounts/check?checkcode="+code+u"</a></h4><br/><p><small>建议您尽早激活。<br/>如果您未找到激活邮件，请查看是否被识别为垃圾邮件，或尝试更换邮箱和重发链接。</small></p><hr/><p>UMUNC</p>")
+	sendmail(to,u"UMUNC邮箱验证",u'''
+		<style type="text/css">
+			table {border: 0px;}
+			table * {border: 0px;}
+			.text-center {align:center;}
+			.head {background:#1C9EFF;color:white;}
+			tr {padding:20px;}
+		</style>
+		<table>
+			<tr>
+				<td class="text-center head">
+					<h3>UMUNC邮箱验证</h3>
+					<span>'''+username+u'''</span>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<p>致 与众不同的你：</p>
+					<p>您收到此邮件是因为此邮箱（ '''+to+u''' ）正在注册“UMUNC IRIS”的账号。</p>
+					<p>如果这不是您的操作，请直接忽视本邮件；如果您确认这是您的操作，请点击下面的地址或将其复制到浏览器地址栏并进入，即可验证您的邮箱，激活账号：</p>
+					<h4><a href=\"http://www.umunc.org/iris/accounts/check?checkcode='''+code+u'''\">http://www.umunc.org/iris/accounts/check?checkcode='''+code+u'''</a></h4>
+				</td>
+			</tr>
+			<tr>
+				<td class="text-center">
+					<p><small>UMUNC</small></p>
+				</td>
+			</tr>
+		</table>
+		''')
