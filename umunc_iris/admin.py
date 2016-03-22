7567909cfb07e506f28e8f9c983f076291bfe812
eastpiger <lvwt@shanghaitech.edu.cn>
2016-03-22 16:45:06 +0800

    fix

--- umunc_iris/admin.py
+++ umunc_iris/admin.py
@@ -64,7 +64,8 @@ class GroupAdmin(ExportActionModelAdmin):
                 </tbody>
             </table>''')
         c = Context({'group': obj})
-        return mark_safe(u'''<a href="#">a</a>''')
+        return mark_safe(u'''
+            <a href="#">a</a>''')
         return t.render(c)
 
     def export_admin_action(self, request, queryset):
