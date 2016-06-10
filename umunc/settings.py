73f2de9f55121957e70692e690be7c1217b2a3b4
eastpiger <lvwt@shanghaitech.edu.cn>
2016-06-10 09:38:27 +0800

    Revert "a"
    This reverts commit 8534017b7f7a5c456226ff585eeb7c5422f93f13.

--- umunc/settings.py
+++ umunc/settings.py
@@ -125,7 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'suit',
+    'suit',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
@@ -133,8 +133,8 @@ INSTALLED_APPS = (
     'umunc_iris',
     'umunc_cheetah',
     'umunc_mpc',
-    # 'import_export',
-    # 'gunicorn',
+    'import_export',
+    'gunicorn',
 )
 
 
