a792c57f044350975af2094eb15f16638ec897a0
eastpiger <lvwt@shanghaitech.edu.cn>
2016-10-05 18:06:51 +0800

    improvement

--- umunc_static/umunc_iris/js/step1.js
+++ umunc_static/umunc_iris/js/step1.js
@@ -1,5 +1,11 @@
 $(function() {
 	$("form  input,select,textarea").jqBootstrapValidation({submitError: function ($form, event, errors) {alert("提交失败！请检查是否存在非法输入！");}});
+	var errorMessages = [];
+$("form  input,select,textarea").each(function (i, el) {
+  errorMessages = errorMessages.concat(
+      $(this).triggerHandler("validation.validation")
+  );
+});
 	$("#submitbutton").click(function(){
 		$('#askModal').modal('hide');
 		$("#submit").val("1");
