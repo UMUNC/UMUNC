5d6c2f1bc475ceb4ed401d6ce2e9426319df43b5
Wentao Lyu <lvwt@shanghaitech.edu.cn>
2016-12-08 12:25:37 +0800

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -3,7 +3,7 @@ python2 manage.py createcachetable
 python2 manage.py makemigrations
 python2 manage.py migrate
 
-nohup gunicorn umunc.wsgi:application -b 127.0.0.1:8080 --workers 4 --worker-connections 65535&
+nohup gunicorn umunc.wsgi:application -b 127.0.0.1:8080 --workers 4 --worker-connections 2048&
 service nginx start
 
 while true
