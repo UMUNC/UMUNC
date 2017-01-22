2081ef46a917428a341f0c1785e975cb86cd377c
eastpiger <lvwt@shanghaitech.edu.cn>
2017-01-22 09:53:58 +0800

    add more control

--- umunc_iris/urls.py
+++ umunc_iris/urls.py
@@ -2,24 +2,42 @@ from django.conf.urls import patterns, include, url
 from view import *
 from umunc import settings
 
-if settings.UMUNC_IRIS_REGISTER:
-    urlpatterns = patterns('',
-        (r'^accounts/login/$', plogin),
-        (r'^accounts/register/$', pregister),
-        (r'^accounts/check/$', pcheck),
-        (r'^accounts/logout/$', plogout),
-        (r'^accounts/change/$', pchange),
-        (r'^$', default),
-        (r'^step1/$', step1),
-        (r'^step2/$', step2),
-        (r'^step3/$', step3),
-        (r'^step4/$', step4),
-        (r'^step5/$', step5),
-        (r'^step6/$', step6),
-        (r'^step7/$', step7),
-        (r'^admin/sendmail/$', sendmail),
-        (r'^admin/markpayment/$', markpayment),
-    )
+if settings.UMUNC_IRIS_ENABLE:
+    if settings.UMUNC_IRIS_REGISTER:
+        urlpatterns = patterns('',
+            (r'^accounts/login/$', plogin),
+            (r'^accounts/register/$', pregister),
+            (r'^accounts/check/$', pcheck),
+            (r'^accounts/logout/$', plogout),
+            (r'^accounts/change/$', pchange),
+            (r'^$', default),
+            (r'^step1/$', step1),
+            (r'^step2/$', step2),
+            (r'^step3/$', step3),
+            (r'^step4/$', step4),
+            (r'^step5/$', step5),
+            (r'^step6/$', step6),
+            (r'^step7/$', step7),
+            (r'^admin/sendmail/$', sendmail),
+            (r'^admin/markpayment/$', markpayment),
+        )
+    else:
+        urlpatterns = patterns('',
+            (r'^accounts/login/$', plogin),
+            (r'^accounts/check/$', pcheck),
+            (r'^accounts/logout/$', plogout),
+            (r'^accounts/change/$', pchange),
+            (r'^$', default),
+            (r'^step1/$', step1),
+            (r'^step2/$', step2),
+            (r'^step3/$', step3),
+            (r'^step4/$', step4),
+            (r'^step5/$', step5),
+            (r'^step6/$', step6),
+            (r'^step7/$', step7),
+            (r'^admin/sendmail/$', sendmail),
+            (r'^admin/markpayment/$', markpayment),
+        )
 else:
     urlpatterns = patterns('',
         (r'^accounts/login/$', plogin),
@@ -28,3 +46,5 @@ else:
         (r'^admin/sendmail/$', sendmail),
         (r'^admin/markpayment/$', markpayment),
     )
+
+
--- umunc_mpc/urls.py
+++ umunc_mpc/urls.py
@@ -2,15 +2,21 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from umunc_mpc.views import *
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'LBlogger.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from umunc import settings
 
-    url(r'^$', view_list),
-    url(r'^list/(.*)/$', view_list),
-    url(r'^post/(\d+)/$', view_post_gloable),
-    url(r'^post/(.*)/(\d+)/$', view_post),
-    url(r'^dashboard/$', dashboard),
-    url(r'^dashboard/edit/(\d)+/$', dashboard_edit),
-)
+if settings.UMUNC_MPC_ENABLE:
+    urlpatterns = patterns('',
+        # Examples:
+        # url(r'^$', 'LBlogger.views.home', name='home'),
+        # url(r'^blog/', include('blog.urls')),
+
+        url(r'^$', view_list),
+        url(r'^list/(.*)/$', view_list),
+        url(r'^post/(\d+)/$', view_post_gloable),
+        url(r'^post/(.*)/(\d+)/$', view_post),
+        url(r'^dashboard/$', dashboard),
+        url(r'^dashboard/edit/(\d)+/$', dashboard_edit),
+    )
+else:
+    urlpatterns = patterns('',
+    )
